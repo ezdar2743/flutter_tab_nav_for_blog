e38790e43c79b374d1e593fe1a8be8de7c1c4488
ezdar <ezdar2743@gmail.com>
2023-12-04 22:36:09 +0900

    ✨add constants

---
+++ lib/constants/fontSizes.dart
@@ -0,0 +1,12 @@
+import 'package:tab_nav/constants/sizes.dart';
+
+class FontSizes {
+  static const xxs = Sizes.size10;
+  static const xs = Sizes.size12;
+  static const s = Sizes.size14;
+  static const m = Sizes.size16;
+  static const l = Sizes.size20;
+  static const xl = Sizes.size24;
+  static const xxl = Sizes.size28;
+  static const xxxl = Sizes.size32;
+}
